4ca561a4e2563369108b9be770d1faa0f4efadf0
Brandon Gatewood <gatew2@pdx.edu>
2024-06-13 16:29:19 -0700

    added footer and some content to pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,16 @@
+// Componets
+import Footer from "../components/Footer";
+
+// Styles
+import "../css/pages/contact.css";
+
 const Contact = () => {
     return(
         <>
-        <h1>Contact</h1>
+        <div className="header">
+            <h1>Contact</h1>
+        </div>
+        <Footer />
         </>
     );
 }
--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,7 +1,25 @@
+// Components
+import Footer from "../components/Footer";
+
+// Styles
+import "../css/pages/faq.css";
+
 const Faq = () => {
     return(
         <>
-        <h1>Faq</h1>
+        <div className="header">
+            <h1>Faq</h1>
+        </div>
+        <div className="moreQuestionsContainer">
+            <p className="question">
+                If you don't find the answer to your question here, please feel free to contact 
+                us directly. We're always happy to help!
+            </p>
+            <a href="/Contact">
+                <button className="contactButton">Book Appointmnet</button>
+            </a>
+        </div>
+        <Footer />
         </>
     );
 }
